8dfbc718ebee32181d214be78708ca1d874c97dd
Andrew Barnett <adbarnett@users.noreply.github.com>
2023-07-14 16:09:59 +0100

    write fizzbuzz method

--- FizzBuzz/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzzService.cs
@@ -1,10 +1,24 @@
-﻿namespace FizzBuzz
+﻿using System.Text;
+
+namespace FizzBuzz
 {
     public class FizzBuzzService : IFizzBuzzService
     {
         public object GetFizzBuzzResponse(int integer)
         {
-            return integer;
+            if (integer % 3 == 0 || integer % 5 == 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                if (integer % 3 == 0)
+                    sb.Append("Fizz");
+
+                if (integer % 5 == 0)
+                    sb.Append("Buzz");
+
+                return sb.ToString();
+            }
+            else
+                return integer;
         }
     }
 }
